aa4fd74c857dd3cca65eb0cdab5b5544d5e0ba2a
thakyZ <950594+thakyZ@users.noreply.github.com>
2023-10-10 15:54:07 -0700

    add: patch project

---
+++ BundledCables/BundledCables.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+using StationeersMods.Interface;
+
+namespace BundledCables {
+  class BundledCables : ModBehaviour {
+    public override void OnLoaded(ContentHandler contentHandler) {
+      //READ THE README FIRST!
+
+      // Harmony harmony = new Harmony("BundledCables");
+      // harmony.PatchAll();
+      UnityEngine.Debug.Log("BundledCables Loaded!");
+    }
+  }
+}
