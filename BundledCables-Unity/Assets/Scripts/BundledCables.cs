94cb3ba0ad80d04c1b4a0ae7ddfaccb92c79c9e8
thakyZ <950594+thakyZ@users.noreply.github.com>
2023-10-11 10:56:39 -0700

    update: minor changes

--- BundledCables-Unity/Assets/Scripts/BundledCables.cs
+++ BundledCables-Unity/Assets/Scripts/BundledCables.cs
@@ -2,12 +2,12 @@ using System;
 using HarmonyLib;
 using StationeersMods.Interface;
 
-public class BundledCables : ModBehaviour
-{
-    public override void OnLoaded(ContentHandler contentHandler)
-    {
-        UnityEngine.Debug.Log("Hello World!");
-        Harmony harmony = new Harmony("BundledCables");
-        harmony.PatchAll();
+namespace BundledCables {
+  public class BundledCables : ModBehaviour {
+    public override void OnLoaded(ContentHandler contentHandler) {
+      UnityEngine.Debug.Log("Hello World!");
+      Harmony harmony = new Harmony("BundledCables");
+      harmony.PatchAll();
     }
+  }
 }
---
+++ BundledCables-Unity/Assets/Scripts/BundledCablesHandler.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using Assets.Scripts.Objects.Electrical;
+using Assets.Scripts.Util;
+
+using Objects.Rockets;
+
+using UnityEngine;
+
+namespace BundledCables {
+  public class BundledCablesHandler : Cable {
+  }
+}
